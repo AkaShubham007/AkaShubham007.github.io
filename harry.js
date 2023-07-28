1bd6ca58650df11b03ed43867bde18edf2bd4215
AkaShubham007 <122309421+AkaShubham007@users.noreply.github.com>
2023-07-29 03:07:16 +0530

    update
    update @29 july

---
+++ harry.js
@@ -0,0 +1,13 @@
+console.log('hello word');
+var a = 78;
+console.log(a)
+var b = "Hello";
+// operator 
+//unary and binary operator 
+var num1 = 56;
+var num2 = 67;
+console.log("The value of num1 + num2 is "+(num1+num2));
+console.log("The value of num1 - num2 is "+(num1-num2));
+console.log("The value of num1 * num2 is "+(num1*num2));
+console.log("The value of num1 / num2 is "+(num1/num2));
+console.log("The value of ++num2 "+(++num2));
